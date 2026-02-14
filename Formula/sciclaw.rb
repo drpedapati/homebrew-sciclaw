7978b76160cbb1ce337a652b580a91dc6bed4b32
Ernie Pedapati <ernest.pedapati@cchmc.org>
2026-02-13 21:45:13 -0500

    Embed release version in sciclaw Homebrew build

--- Formula/sciclaw.rb
+++ Formula/sciclaw.rb
@@ -8,7 +8,8 @@ class Sciclaw < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(output: bin/"sciclaw"), "./cmd/picoclaw"
+    ldflags = "-s -w -X main.version=v#{version}"
+    system "go", "build", *std_go_args(output: bin/"sciclaw", ldflags: ldflags), "./cmd/picoclaw"
     (bin/"picoclaw").make_symlink bin/"sciclaw"
     pkgshare.install "skills"
     (pkgshare/"templates"/"workspace").install Dir["pkg/workspacetpl/templates/workspace/*.md"]
@@ -17,5 +18,6 @@ class Sciclaw < Formula
   test do
     assert_match "Usage:", shell_output("#{bin}/sciclaw --help")
     assert_match "Usage:", shell_output("#{bin}/picoclaw --help")
+    assert_match "v#{version}", shell_output("#{bin}/sciclaw --version")
   end
 end
